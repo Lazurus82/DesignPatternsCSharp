94320933be3d3fd179a5ebd38d964af25b565146
E.P, Nelson <42788428+Lazurus82@users.noreply.github.com>
2018-09-18 21:09:39 -0500

    Base files for Design Patterns

---
+++ DesignPatternsClass/DesignPatternsClass/Base/Vintage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsClass
+{
+   public class Vintage : AbstractRoadBike
+    {
+        public Vintage()
+          : this(BikeColor.Green)
+        {
+
+        }
+        public Vintage(BikeColor color)
+             : base(color)
+
+        {
+
+        }
+
+
+    }
+}
